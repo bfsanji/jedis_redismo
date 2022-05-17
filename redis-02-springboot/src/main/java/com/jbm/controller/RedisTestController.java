7e0eb9c5ed1303d76bde4c916bfa61968d71c3bc
JBM <JBM@qq.com>
2022-05-17 20:07:10 +0800

    first commit

--- redis-02-springboot/src/main/java/com/jbm/controller/RedisTestController.java
+++ redis-02-springboot/src/main/java/com/jbm/controller/RedisTestController.java
@@ -14,6 +14,7 @@ public class RedisTestController {
     public String testRedis(){
         redisTemplate.opsForValue().set("name","luvy");
         String name = (String)redisTemplate.opsForValue().get("name");
+        System.out.println(name);
         return name;
     }
 }
